9ab4c90aafd5e5bfd7fe5d2b6f580a3ba1e8fd26
Phirery <dat123456789fa@gmail.com>
2026-02-26 01:10:47 +0700

    Delete cloudinary.php

--- src/config/cloudinary.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'cloud_name' => 'dlnevod7e',   // Thay bằng Cloud name của bạn
-    'api_key'    => '794121161892298',       // Thay bằng API Key
-    'api_secret' => 'hIUlZBVnsNNPW0cUmPcTxDFXLeg',    // Thay bằng API Secret
-    'folder'     => 'Images' // Thư mục lưu ảnh trên Cloudinary
-];
